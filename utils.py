6e4ebb4fa7868bdd12c529afe78ca97063204c6f
Martin Pellizzer <martinpellizzer@gmail.com>
2025-03-05 22:35:18 +0100

    '

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
-def get_socket_x(node, side):
-    if side == 'input': x = node['x']
-    elif side == 'output': x = node['x'] + node['w']
+def get_socket_x(node, side, camera):
+    if side == 'input': x = node['x'] + camera['x']
+    elif side == 'output': x = node['x'] + node['w'] + camera['x']
     return x
 
-def get_socket_y(node, i=0):
-    y = node['y'] + 32*i
+def get_socket_y(node, i, camera):
+    y = node['y'] + 32*i + camera['y']
     return y
 
